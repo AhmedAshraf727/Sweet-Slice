b342613cd06dbb88b8f6fa994cf953b5297795ce
Ahmed Ashraf <134469174+AhmedAshraf727@users.noreply.github.com>
2026-05-05 19:17:16 +0200

    fix db init

--- app.py
+++ app.py
@@ -14,7 +14,9 @@ app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
-
+with app.app_context():
+    db.create_all()
+    
 # تعريف موديل الطلبات
 class Order(db.Model):
     id = db.Column(db.Integer, primary_key=True)
